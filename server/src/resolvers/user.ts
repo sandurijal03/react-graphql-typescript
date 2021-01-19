b159ec4c5b3f2d55b38ef45eb0df1e9c50342cb0
sandip rijal <sandurijal03@hotmail.com>
2021-01-20 03:01:41 +0545

    adding sessssion part and query for logged in user

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from 'type-graphql';
 import argon2 from 'argon2';
@@ -39,10 +40,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // u are not loggedin
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext,
+    @Ctx() { em, req }: MyContext,
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -85,13 +96,16 @@ export class UserResolver {
         };
       }
     }
+
+    req.session.userId = user.id;
+
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext,
+    @Ctx() { em, req }: MyContext,
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -116,6 +130,9 @@ export class UserResolver {
         ],
       };
     }
+
+    req.session.userId = user.id;
+
     return { user };
   }
 }
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,9 @@
 import { Connection, EntityManager, IDatabaseDriver } from '@mikro-orm/core';
+import { Request, Response } from 'express';
+import { Session } from 'express-session';
 
 export type MyContext = {
+  req: Request & { session?: Session & { userId?: number } };
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  res: Response;
 };
